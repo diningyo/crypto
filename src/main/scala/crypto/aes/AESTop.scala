c311f894ef04d06c7fca3665a6b9c13008f6e28c
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-10-27 10:20:47 +0900

    add AESTop module (only definition for making test environment).

---
+++ src/main/scala/crypto/aes/AESTop.scala
@@ -0,0 +1,35 @@
+// See LICENSE for license details.
+
+package crypto.aes
+
+import chisel3._
+import chisel3.experimental.ChiselEnum
+import chisel3.util._
+
+/**
+ * AES mode
+ */
+object AESMode extends ChiselEnum {
+  val enc, dec = Value
+}
+
+
+/**
+ * AES I/O
+ */
+class AESTopIO extends Bundle {
+  val key = UInt(256.W)
+  val nk = UInt(log2Ceil(8).W)
+  val mode = AESMode
+  val data_in = Flipped(DecoupledIO(UInt(256.W)))
+  val data_out = DecoupledIO(UInt(256.W))
+}
+
+/**
+ * AES top module
+ */
+class AESTop extends Module {
+  val io = IO(new AESTopIO)
+
+  io := DontCare
+}

e7d52cc0883235489b4a6471bda0151c7b57c377
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-10-27 10:33:33 +0900

    bug fixed.

--- src/main/scala/crypto/aes/AESTop.scala
+++ src/main/scala/crypto/aes/AESTop.scala
@@ -13,14 +13,13 @@ object AESMode extends ChiselEnum {
   val enc, dec = Value
 }
 
-
 /**
  * AES I/O
  */
 class AESTopIO extends Bundle {
-  val key = UInt(256.W)
-  val nk = UInt(log2Ceil(8).W)
-  val mode = AESMode
+  val key = Input(UInt(256.W))
+  val nk = Input(UInt(log2Ceil(8).W))
+  val mode = Input(UInt(AESMode.getWidth.W))
   val data_in = Flipped(DecoupledIO(UInt(256.W)))
   val data_out = DecoupledIO(UInt(256.W))
 }

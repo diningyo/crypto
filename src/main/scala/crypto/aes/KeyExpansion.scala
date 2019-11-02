a8c81addddc0ac404e41315a0e2f23ed0f7286e9
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-11-02 12:12:08 +0900

    change interface UInt to Vec[UInt].

--- src/main/scala/crypto/aes/KeyExpansion.scala
+++ src/main/scala/crypto/aes/KeyExpansion.scala
@@ -9,8 +9,8 @@ import chisel3._
  */
 class KeyExpansion extends Module {
   val io = IO(new Bundle {
-    val key_in = Input(UInt(256.W))
-    val key_out = Output(UInt(256.W))
+    val key_in = Input(Vec(32, UInt(8.W)))
+    val key_out = Output(Vec(32, UInt(8.W)))
   })
 
   val rcon = VecInit(Seq(0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36).map(_.U))
